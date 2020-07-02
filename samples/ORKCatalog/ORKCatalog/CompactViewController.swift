c2e9203baad610e3c94d747ab3640849acf07c9f
songchong <songchong@maxour.com>
2020-07-03 07:08:04 +0900

    add Compact View

---
+++ samples/ORKCatalog/ORKCatalog/CompactViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CompactViewController.swift
+//  ORKCatalog
+//
+//  Created by 宋チュウ on 2020/07/02.
+//  Copyright © 2020 researchkit.org. All rights reserved.
+//
+
+import UIKit
+
+class CompactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
