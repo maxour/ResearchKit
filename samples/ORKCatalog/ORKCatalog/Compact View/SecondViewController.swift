4175cbc4d7541505ca3226a2d58e2655442226d9
songchong <songchong@maxour.com>
2020-07-13 17:55:14 +0900

    second page can scroll

--- samples/ORKCatalog/ORKCatalog/Compact View/SecondViewController.swift
+++ samples/ORKCatalog/ORKCatalog/Compact View/SecondViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var centerStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var newView: UIView
+        for row in 0...60 {
+            if (row % 2 == 0) {
+                newView = SimpleSegmentedView()
+                centerStackView.addArrangedSubview(newView)
+            } else {
+                newView = FullSegmentedView()
+                centerStackView.addArrangedSubview(newView)
+            }
+            
+        }
     }
     
 
